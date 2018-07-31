75b29da6cada7cd9115529f07144380a15e94d15
Ray <ray00000sina@gmail.com>
2018-07-31 22:00:34 +0800

    fixed : 修改各class method init()的命名符合code style.

--- src/main/java/link/Role.java
+++ src/main/java/link/Role.java
@@ -3,11 +3,44 @@ package link;
 import link.base.RoleBase;
 
 public class Role extends RoleBase {
-	public int mMazeX;
-	public int mMazeY;
+	private int mMazeX;
+	private int mMazeY;
+	private int mKind; // 角色類別,1:背景,2:按鈕,3:牌
+	private boolean mIsFoucs;
 
 	public Role() {
 
 	}
 
+	public void setKind(int k) {
+		mKind = k;
+	}
+
+	public int getKind() {
+		return mKind;
+	}
+
+	public void setFocus(boolean b) {
+		mIsFoucs = b;
+	}
+
+	public boolean isFocus() {
+		return mIsFoucs;
+	}
+
+	public int getMazeX() {
+		return mMazeX;
+	}
+
+	public void setMazeX(int mMazeX) {
+		this.mMazeX = mMazeX;
+	}
+
+	public int getMazeY() {
+		return mMazeY;
+	}
+
+	public void setMazeY(int mMazeY) {
+		this.mMazeY = mMazeY;
+	}
 }
